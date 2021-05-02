2c759c022dacace106e704c54263318c0eb25430
sahil pathania <65562763+sahilpathania23@users.noreply.github.com>
2021-05-02 15:32:46 +0530

    Add files via upload
    parameterized constructor in c++

---
+++ parameterconstructor.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+// first we have to create a blue print 
+class car{
+public:
+	int price;
+	char name[100];
+	int average;
+	int model;
+	void print(){
+		cout << "name :"<< name << endl;
+	cout<< "average :" << average << endl;
+	cout<<"model :" << model << endl;
+	cout << "price :"<< price << endl;
+
+	}
+	car(){
+		cout<<"this is a default constructor"<<endl;
+	}
+	car(char *p,int n,int m,int i){
+		cout<<"this is a parameterized constructor"<<endl;
+		strcpy(name,p);
+		price=n;
+		model=m;
+		average=i;
+
+	}
+
+
+};
+
+
+int main(){
+	car a;
+	// a.name="bwm"; this will not work
+	strcpy(a.name,"BMW");
+	a.price=100;
+	a.average=10;
+	a.model=2;
+	a.print();
+	car b("SANTRO",130,13,12);
+	// cout << a.name << endl;
+	// cout << a.average << endl;
+	// cout << a.model << endl;
+	// cout << a.price << endl;
+	// this above can be done using functiond
+
+
+	// strcpy(b.name,"AUDI");
+	// b.price=103;
+	// b.average=130;
+	// b.model=221;
+	// b.print();
+
+
+
+	
+	b.print();
+
+	return 0;
+
+}
